b3e8c4333fd5435541ec2d0b7f9910d9a4fbf73c
unknown <mateuszkowalczyk7@gmail.com>
2023-01-21 14:35:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const selectedColor = document.getElementById("favcolor")
+const main = document.querySelector('main')
+const colorsWrapper = document.getElementById("colorsWrapper")
+const colorsHexWrapper = document.getElementById("colorsHexWrapper")
+const typeSelector = document.getElementById("typeSelector")
+
+let colorType = ``
+let colorsArr = []
+let myColor = ``
+
+function getColor(){
+    myColor = selectedColor.value
+    return myColor
+}
+
+selectedColor.addEventListener("change", getColor)
+
+document.getElementById("submitBtn").addEventListener("click", function(){
+    getColor()
+    api()
+})
+
+function api(){
+    let colorType = typeSelector.value.toLowerCase()
+    let apiColor = myColor.slice(1)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${apiColor}&mode=${colorType}`)
+    .then((response) => response.json())
+    .then(function(data){
+        colorsArr = data.colors
+        getHex()
+    })
+}
+
+function getHex(){
+    colorsWrapper.innerHTML = ``
+    colorsHexWrapper.innerHTML = ``
+    for(let i = 0; i < colorsArr.length; i++){
+         
+        colorsWrapper.innerHTML += `<div class="color" style="background-color: ${colorsArr[i].hex.value};"></div>` 
+        
+        colorsHexWrapper.innerHTML +=`<p class="hexParagraph">${colorsArr[i].hex.value}</p>`
+
+    }
+}
